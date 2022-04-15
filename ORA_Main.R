a1f147457b190dced45fcc0d420873a5f00a61f2
CharleneMSI <q2330416@gmail.com>
2022-04-15 18:27:44 +0800

    Clean up the code.

--- ORA_Main.R
+++ ORA_Main.R
@@ -1,50 +1,72 @@
-#???Over-Representation Analysis with ClusterProfiler
+## Over-Representation Analysis with ClusterProfiler
 #  https://learn.gencore.bio.nyu.edu/rna-seq-analysis/over-representation-analysis/
 
-# Install and load packages
-  # BiocManager::install("clusterProfiler")
-  # BiocManager::install("pathview")
-  # install.packages("wordcloud")
-  library(clusterProfiler)
+##### Presetting ######
+  rm(list = ls()) # Clean variable
+  memory.limit(150000)
+
+##### Load Packages  #####   
+  #### Install and load packages ####
+    # BiocManager::install("clusterProfiler")
+    # BiocManager::install("pathview")
+    # BiocManager::install("enrichplot")
+    # BiocManager::install("ggupset")
+    # BiocManager::install(organism, character.only = TRUE)
+
+    # install.versions(c('RColorBrewer'), c('1.1.2'))
+
+  #### Load Packages ####
+    library(clusterProfiler)
+    library(enrichplot)
+    library(ggupset)
+    # Annotations
+    organism = "org.Dm.eg.db"
+    library(organism, character.only = TRUE)
   
-# Annotations
-  organism = "org.Dm.eg.db"
-  #BiocManager::install(organism, character.only = TRUE)
-  library(organism, character.only = TRUE)
+  ##### Function setting  ##### 
+    ## Call function
+    source("FUN_Beautify_ggplot.R")
+    source("FUN_GSEA_ggplot.R")
+  
+  ##### Current path and new folder setting  ##### 
+    Version = paste0(Sys.Date(),"_","ORA")
+    Save.Path = paste0(getwd(),"/",Version)
+    dir.create(Save.Path)
+  
 
 ##### Prepare Input #####
-  # reading in input from deseq2
+  ## reading in input from deseq2
   df = read.csv("drosphila_example_de.txt", header=TRUE)
   
-  # we want the log2 fold change 
+  ## we want the log2 fold change 
   original_gene_list <- df$log2FoldChange
   
-  # name the vector
+  ## name the vector
   names(original_gene_list) <- df$X
   
-  # omit any NA values 
-  gene_list<-na.omit(original_gene_list)
+  ## omit any NA values 
+  gene_list <- na.omit(original_gene_list)
   
-  # sort the list in decreasing order (required for clusterProfiler)
+  ## sort the list in decreasing order (required for clusterProfiler)
   gene_list = sort(gene_list, decreasing = TRUE)
   
-  # Exctract significant results (padj < 0.05)
+  ## Exctract significant results (padj < 0.05)
   sig_genes_df = subset(df, padj < 0.05)
   
-  # From significant results, we want to filter on log2fold change
+  ## From significant results, we want to filter on log2fold change
   genes <- sig_genes_df$log2FoldChange
   
-  # Name the vector
+  ## Name the vector
   names(genes) <- sig_genes_df$X
   
-  # omit NA values
+  ## omit NA values
   genes <- na.omit(genes)
   
-  # filter on min log2fold change (log2FoldChange > 2)
+  ## filter on min log2fold change (log2FoldChange > 2)
   genes <- names(genes)[abs(genes) > 2]
 
 ##### Create enrichGO object #####  
-  # Create the object
+  ## Create the object
   go_enrich <- enrichGO(gene = genes,
                         universe = names(gene_list),
                         OrgDb = organism, 
@@ -56,10 +78,6 @@
   
 ##### Output #####
   ## Upset Plot
-  # BiocManager::install("enrichplot")
-  # BiocManager::install("ggupset")
-  library(enrichplot)
-  library(ggupset)
   upsetplot(go_enrich)
 
   ## Barplot
@@ -83,10 +101,10 @@
   cnetplot(go_enrich, categorySize="pvalue", foldChange=gene_list)
   
 
-# ##### Error part #####
+# ##### Error part (to be corrected) #####
 #   
 #   ##### Wordcloud #####
-#   # install.versions(c('RColorBrewer'), c('1.1.2'))
+#   # install.packages("wordcloud")
 #   library(wordcloud)
 #   
 #   ## Wordcloud
